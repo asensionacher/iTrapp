fce87f105dfa1c2eee9884af2f019cc59d07f893
Sergi Asensio <sergi.asensio.nacher@gmail.com>
2014-09-29 15:55:26 +0200

    Initial Commit

---
+++ iTrapp/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  iTrapp
+//
+//  Created by Sergi Asensio on 29/09/14.
+//  Copyright (c) 2014 Sergi Asensio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ iTrapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iTrapp
+//
+//  Created by Sergi Asensio on 29/09/14.
+//  Copyright (c) 2014 Sergi Asensio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
